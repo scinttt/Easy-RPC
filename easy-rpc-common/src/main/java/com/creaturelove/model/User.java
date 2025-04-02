a1eb2129bff2e99c3dd736743010fdf1afe59884
scinttt <scinttt@gmail.com>
2025-04-02 15:21:07 -0400

    Make User Entity implements Serializable & Delete Main class

--- easy-rpc-common/src/main/java/com/creaturelove/model/User.java
+++ easy-rpc-common/src/main/java/com/creaturelove/model/User.java
@@ -1,6 +1,8 @@
 package com.creaturelove.model;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
     private String name;
 
     public String getName() {
